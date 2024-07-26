88696534233852753008a95c8b94eb7c4a49d497
E-Zbox <zboxstudios97@gmail.com>
2024-07-26 18:26:45 +0100

    added socket handling when webhook gets called

--- nft-activity-tracker/backend/src/controllers/index.ts
+++ nft-activity-tracker/backend/src/controllers/index.ts
@@ -1,13 +1,39 @@
 import { NextFunction, Request, Response } from "express";
+// app
+import { io } from "@/app";
+// event listeners
+import { emitEvents, onEvents } from "@/listeners";
 
 export const webhookController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const matchedTransactions = req.body;
   console.log("json body");
-  console.log(JSON.stringify(req.body));
+  console.log(JSON.stringify(matchedTransactions));
   console.log("------- json body ---------");
+
+  const { send_test_success } = emitEvents;
+
+  const { send_test } = onEvents;
+
+  const addressForClientToListen = [];
+
+  // if (typeof matchedTransactions ===)
+
+  io.on("connection", (socket) => {
+    console.log(socket);
+
+    socket.on(send_test, (payload) => {
+      socket.emit(send_test_success, {
+        message: "connected to socket",
+        success: true,
+      });
+    });
+
+    io.emit("got_some_data", { message: "hey there" });
+  });
 
   return res.status(200).json({ data: null, error: "", success: true });
 };
