63cb3c9c3138b815a07de552fc4fc03928b6e3be
E-Zbox <zboxstudios97@gmail.com>
2024-07-26 19:47:11 +0100

    updated handling of streamed data

--- nft-activity-tracker/backend/src/controllers/index.ts
+++ nft-activity-tracker/backend/src/controllers/index.ts
@@ -33,19 +33,24 @@ export const webhookController = async (
   matchedTransactions.forEach((tx) => {
     const { from, to } = tx;
 
-    if (from !== to) {
+    const _from = from.toLowerCase();
+    const _to = to.toLowerCase();
+
+    if (_from !== _to) {
       addressForClientToListen.push(to.toLowerCase());
     }
 
     addressForClientToListen.push(from.toLowerCase());
   });
 
+  let response = {
+    data: Array.from(new Set(addressForClientToListen)),
+    error: "",
+    success: true,
+  };
+
   if (addressForClientToListen.length > 0) {
-    io.emit(updated_watch_address, {
-      data: addressForClientToListen,
-      error: "",
-      success: true,
-    });
+    io.emit(updated_watch_address, response);
   }
 
   io.emit("streams_timestamp", new Date().toISOString());
