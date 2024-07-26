32bc10cd337d83f5c4532df7bb081317f87a85a3
E-Zbox <zboxstudios97@gmail.com>
2024-07-26 08:36:11 +0100

    added webhook controller to routes

--- nft-activity-tracker/backend/src/routes/index.ts
+++ nft-activity-tracker/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+// controllers
+import { webhookController } from "@/controllers";
 
 const routes = Router();
 
-routes.post("");
+routes.post("/webhook", webhookController);
 
 export const baseURL = "/api/v1";
 
