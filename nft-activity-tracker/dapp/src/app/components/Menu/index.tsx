d77b8403504ece4683c48d45268855615aed59ea
E-Zbox <zboxstudios97@gmail.com>
2024-08-02 21:51:20 +0100

    added logic for searchbar to update notification expression

--- nft-activity-tracker/dapp/src/app/components/Menu/index.tsx
+++ nft-activity-tracker/dapp/src/app/components/Menu/index.tsx
@@ -1,9 +1,11 @@
 "use strict";
+import { ChangeEvent, useEffect, useState } from "react";
 // components
 import SelectedAddress from "./SelectedAddress";
 // store
-import { useAppStore } from "@/store";
+import { useAppStore, useTransactionStore } from "@/store";
 // styles
+import { Loader } from "@/app/styles/Loader.styles";
 import {
   MainMenu,
   MainSearch,
@@ -11,17 +13,17 @@ import {
   SearchInput,
   SearchLogo,
 } from "../../styles/Menu.styles.tsx";
-// utils
-import { screens } from "@/utils/data";
 import {
   FlexContainer,
   ScrollerContainer,
 } from "../../styles/shared/Container.styles";
+// utils
+import { screens } from "@/utils/data";
 
 const Menu = () => {
   const {
     default: {
-      assets: { searchIcon },
+      assets: { loaderGif, searchIcon },
     },
   } = screens;
 
@@ -30,12 +32,60 @@ const Menu = () => {
     showMenu,
   }));
 
+  const { loading, searchQuery, setSearchQuery } = useTransactionStore(
+    ({ loading, searchQuery, setSearchQuery }) => ({
+      loading,
+      searchQuery,
+      setSearchQuery,
+    })
+  );
+
+  const [formState, setFormState] = useState({ input_search: "" });
+
+  const handleInputChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    if (loading && searchQuery == formState.input_search) {
+      return;
+    }
+
+    setFormState({ input_search: value });
+  };
+
+  const handleSubmit = () => {
+    const { input_search: searchText } = formState;
+
+    if (searchText.length == 0 || loading) {
+      return;
+    }
+
+    const regex = /^0x[a-fA-F0-9]{40}$/;
+
+    if (!regex.test(searchText)) {
+      alert(`"${searchText}" is not a valid Ethereum wallet address!`);
+      return;
+    }
+
+    setSearchQuery(searchText);
+  };
+
   return (
     <MainMenu $showMenu={showMenu}>
       <MenuTitle>Alert Subscription</MenuTitle>
       <MainSearch>
-        <SearchLogo src={searchIcon.src} />
-        <SearchInput placeholder="Filter address subscriptions..." />
+        {loading && searchQuery === formState.input_search ? (
+          <Loader src={loaderGif.src} $size="40px" />
+        ) : (
+          <SearchLogo src={searchIcon.src} onClick={handleSubmit} />
+        )}
+        <SearchInput
+          id="input_search"
+          name="input_search"
+          placeholder="Filter address subscriptions..."
+          value={formState.input_search}
+          onChange={handleInputChange}
+          onSubmit={handleSubmit}
+        />
       </MainSearch>
       <ScrollerContainer
         $bgColor="#4442"
